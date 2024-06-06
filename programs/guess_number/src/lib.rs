2bc3fad9b1ded67f979cc129c94280d09bd4146b
Keeper <littlesevenyhf@gmail.cp>
2024-06-06 23:47:09 +0800

    guess_number logic

--- programs/guess_number/src/lib.rs
+++ programs/guess_number/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use solana_program::clock::Clock;
 
 declare_id!("4GXP1uFLNxwgLjfCf3WbT3stTLjunr1rxepukF77YxG6");
 
@@ -6,10 +7,68 @@ declare_id!("4GXP1uFLNxwgLjfCf3WbT3stTLjunr1rxepukF77YxG6");
 pub mod guess_number {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    use std::cmp::Ordering;
+
+    pub fn initialize(ctx: Context<AccountContext>) -> Result<()> {
+        let guessing_account = &mut ctx.accounts.guessing_account;
+        guessing_account.number = generate_random_number();
         Ok(())
+    }
+
+    pub fn guess(ctx: Context<AccountContext>, number: u32) -> Result<()> {
+        let guessing_account = &mut ctx.accounts.guessing_account;
+        let target = guessing_account.number;
+
+        match number.cmp(&target) {
+            Ordering::Less => {
+                return err!(MyError::NumberTooSmall)
+            }
+            Ordering::Greater => {
+                return  err!(MyError::NumberTooLarge);
+            }
+            Ordering::Equal => {
+                return Ok(())
+            }
+        }
     }
 }
 
+fn generate_random_number() -> u32 {
+    let clock = Clock::get().expect("Failed to get clock");
+    let last_digit = (clock.unix_timestamp % 10) as u8;
+    let result = (last_digit + 1) as u32;
+    result
+}
+
+#[account]
+pub struct GuessingAccount {
+    pub number: u32,
+}
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct AccountContext<'info> {
+
+    #[account(
+        init_if_needed,
+        space=32,
+        payer=payer,
+        seeds = [b"guessing pda"],
+        bump
+    )]
+    pub guessing_account: Account<'info, GuessingAccount>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+
+#[error_code]
+pub enum MyError {
+    #[msg("Too small")]
+    NumberTooSmall,
+    #[msg("Too larget")]
+    NumberTooLarge
+}
